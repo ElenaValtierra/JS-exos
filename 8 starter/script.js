7d7566bb6fce128ee39cf08e5e0489e07d0a2aa2
evaltierra <elena.valtierra@sjsolutions.net>
2023-02-02 14:20:54 +0100

    Shallow Copy of objects

--- 8 starter/script.js
+++ 8 starter/script.js
@@ -37,18 +37,18 @@
 
 // console.log(calcAge(1995));
 
-var me = 'Elena';
-let job = 'superstar';
-const year = 1995;
+// var me = 'Elena';
+// let job = 'superstar';
+// const year = 1995;
 
-// Functions
-console.log(addDecl(2,3));
-// console.log(addExpr(2,3));
-// console.log(addArrow(2,3));
+// // Functions
+// console.log(addDecl(2,3));
+// // console.log(addExpr(2,3));
+// // console.log(addArrow(2,3));
 
-function addDecl(a,b){
-    return a+b;
-}
+// function addDecl(a,b){
+//     return a+b;
+// }
 
 // // It will be saved in the pc memory (hoisting) but it is unaccessible and so if it is called before declaration it will not be found
 // // Var will appear as undefined
@@ -93,22 +93,99 @@ function addDecl(a,b){
 // }
 // console.log(calcAgeArrow(1990)); 
 
-const elena = {
-    year: 1995,
-    calcAge: function(){
-        console.log(this); // elena object // if cristina.calcAge() is called after then it will be 17, as it is cristina's
-        console.log(2037 - this.year);
-    }
-}
+// const elena = {
+//     year: 1995,
+//     calcAge: function(){
+//         console.log(this); // elena object 
+//         console.log(2037 - this.year);
+//     }
+// }
 
-console.log(elena.calcAge());
+// console.log(elena.calcAge());
 
-const cristina = {
-    year: 2017
+// const cristina = {
+//     year: 2017
+//             // if cristina.calcAge() is called after then it will be 17, as it is cristina's
+// };
+
+// // copy frrom the object elena the method => method borrowing
+// cristina.calcAge = elena.calcAge;
+// console.log(cristina);
+// console.log(cristina.calcAge());// this will be cristina as it is the object that calls the method
+
+// var firstName = 'Mathilda';
+
+// const elena = {
+//     firstName: 'Elena',
+//     year: 1995,
+//     calcAge: function(){
+//         //console.log(this); // elena object 
+//         console.log(2037 - this.year);
+//         const isMillenial = () =>{ // ? Use arrow function as the parent is another function
+//             console.log(this);
+//             console.log(this.year >= 1981 && this.year <= 1996);
+//         }
+//         isMillenial();
+//     },
+//     greet: ()=> {// ! do not use arrow function here as it uses the scope
+//         console.log(this); // global window
+//         console.log(`Hey ${this.firstName}`)} // var goes to the global object so it has access here, do not ude!
+// }
+
+// elena.greet();
+// elena.calcAge(); 
+
+// * *** Primitives vs objects ***
+
+// const me = {
+//     name: 'Elena',
+//     age: 27
+// }
+
+// const friend = me;
+
+// console.log(me);
+// console.log(friend);
+
+// friend.age = 30;
+// console.log('Me: ',me);
+// console.log('Friend: ',friend);
+
+// Primitive types
+let lastName = 'Williams';
+let oldLastName = lastName;
+
+lastName = 'Davis';
+console.log(lastName, oldLastName);
+
+// Reference types
+const jessica = {
+    firstName: 'Jessica',
+    lastName: 'Williams',
+    age : 27
 };
 
-// copy frrom the object elena the method => method borrowing
-cristina.calcAge = elena.calcAge;
-console.log(cristina);
-console.log(cristina.calcAge());// this will be cristina as it is the object that calls the method
+const marriedJessica = jessica;
 
+marriedJessica.lastName = 'Davis';
+
+// console.log('Before marriage', jessica);
+// console.log('After marriage', marriedJessica);
+
+// Copying objects
+
+const jessica2 = {
+    firstName: 'Jessica',
+    lastName: 'Williams',
+    age : 27,
+    family: ['Alice', 'Bob']
+};
+
+const jessicaCopy = Object.assign({}, jessica2); // properties copied from the original // ? shallow copy made (copies the first level not the array if modified)
+jessicaCopy.lastName = 'Davis';
+jessicaCopy.family.push('Maxime', 'Antoine');
+
+console.log('Before marriage', jessica2);
+console.log('After marriage', jessicaCopy);
+
+// todo Deep clone with libary
