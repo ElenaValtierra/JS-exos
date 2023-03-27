e0b2ad70fbc15af6038469c2091d763976617e6c
evaltierra <elena.valtierra@sjsolutions.net>
2023-03-27 13:14:16 +0200

    Functions

---
+++ 10 starter/script.js
@@ -0,0 +1,61 @@
+'use strict';
+/*
+const bookings =[];
+
+// the numPassenger=1 '=' means that if it is indefined it will take the value that has been assigned
+const createBooking = (flightName, numPassenger=1, price=199 * numPassenger) =>{
+    const booking = {
+        flightName,
+        numPassenger,
+        price
+    }
+    console.log(booking);
+    bookings.push(booking);
+}
+
+createBooking('LH123'); // will take the default values as it is falsely
+createBooking('LG458', 4, 688); // we give all the values
+createBooking('LG058', 4); // will get the last falsely value that has been assigned
+createBooking('LG058',undefined, 400); // we skip a parameter when we say it is undefined BUT we have to specify it
+
+*/
+
+// --------------
+
+const flight = 'LH123';
+
+const elena = {
+    name: 'Elena Valtierra',
+    passport: 545641516588
+}
+
+const checkIn = (flightNum, passager)=>{
+    flightNum = 'LHGGG'; // we reassign -> creates a COPY
+    passager.name = 'Ms. ' + passager.name; // the object elena will be modified
+// if(passager.passport === 545641516588){
+//     alert('Checked in')
+// }else{
+//     alert('Wrong passport!')
+// }
+};
+
+checkIn(flight, elena);
+console.log(flight); // not modified
+console.log(elena); // modified
+
+
+
+// Passing a primitive type to a function is the same as creating a copy outside of the function, the VALUE is copied (const flightNum = flight;)
+//* when we pass an object to a function, it is like copying an object if we change it in the copy it will change in the original
+
+// Passing by value & passing by reference(does not exist in JS)
+
+
+// * Fiest-class function 
+// Stores functions in variables/ properties
+// pass functions as arguments to OTHER functions
+// Return functions from functions
+// Call methods on functions
+
+// * Higher-order functions
+// A function that receives another function as an argument, that returns a new function or both, thanks to the first-class functions
