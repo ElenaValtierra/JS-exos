632f86ad61d9bd437483614bf38bcc178d0c7ddd
Elena <elena.valt995@gmail.com>
2023-05-24 15:56:58 +0200

    event delegation part 1

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -1,12 +1,16 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
+
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+///////////////////////////////////////
+// Modal window
 
 const openModal = function(e) {
     e.preventDefault();
@@ -32,120 +36,8 @@ document.addEventListener('keydown', function(e) {
     }
 });
 
-//  //::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-// Selecting elements
-// console.log(document.documentElement); // all dom
-// console.log(document.head);
-// console.log(document.body);
-
-
-// // console.log();
-const header = document.querySelector('.header');
-const allSections = document.querySelectorAll('.section');
-
-// document.getElementById('section--1');
-// const allBtns = document.getElementsByTagName('button');
-// console.log(allBtns);
-
-//*  Creating and inserting Elements
-// .insertAdjacentHTML method takes two parameters: the position where you want to insert the HTML content and the actual HTML content you want to insert
-/*
-'beforebegin': Inserts the HTML content immediately before the element.
-'afterbegin': Inserts the HTML content at the beginning of the element.
-'beforeend': Inserts the HTML content at the end of the element.
-'afterend': Inserts the HTML content immediately after the element.
-*/
-//  Exemple
-// const element = document.getElementById('myElement');
-// const htmlContent = '<p>This is some text.</p>';
-
-// element.insertAdjacentHTML('afterend', htmlContent);
-
-
-//? The div has been created but it is not present in the DOM 
-const message = document.createElement('div');
-message.classList.add('cookie-message');
-
-// message.textContent = 'We use cookies for improved functionality and analytics';
-
-message.innerHTML =
-    'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
-
-// ? We add the div after the header so that it will be present in the DOM - first child in header
-
-//?DOM elements are unique and can only exist in 1 place at a time, so it will prepend and then move to the append - Except if we copy it using clineNode(true) - true for the child elements
-
-// CHILD
-// header.prepend(message); // child beginning
-header.append(message); // child end
-// header.append(message.cloneNode(true)); // child end
-
-// SIBLINGS
-// header.before(message); // sister before element
-// header.after(message); // sister after element
-
-// Delete Elements
-document.querySelector('.btn--close-cookie').addEventListener('click', function() {
-    message.remove();
-    // Before they use to do DOM traversing to access the element, as you could not access it directly
-    // message.parentElement.removeChild(message);
-});
-
-// -------------------------------
-// * Style, attributes and classes
-
-// Styles
-message.style.backgroundColor = '#37383d';
-message.style.width = '120%';
-
-console.log(message.style.color);
-
-// To get a style from an element - getComputedStyle()
-console.log(getComputedStyle(message).color);
-console.log(getComputedStyle(message).height);
-
-// ! the height is not taken into account, idk why
-message.style.height =
-    Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
-
-
-// ? CUSTOM PROPERTIES - setProperty is the :root in .css to change several things at the same time
-document.documentElement.style.setProperty('--color-primary', 'pink')
-
-
-//* Attributes
-const logo = document.querySelector('.nav__logo');
-//* Classic ones - existe déjà
-console.log(logo.alt);
-console.log(logo.src); // shows the absolute link (same in href), you need a getAttribute to get the relative one, as show below
-console.log(logo.getAttribute('src'));
-console.log(logo.className);
-// setting them:
-logo.alt = 'Bautiful minimalist logo';
-
-//* Non-standard - created by me
-console.log(logo.designer); // 'undefined' - Not the way to access it as it is me that created this attribute
-console.log(logo.getAttribute('designer')); // 'Elena' - correct way to get it
-// setting them:
-logo.setAttribute('company', 'Bankist');
-
-//  dataset - to get from the attribute data : data-version-number="3.0", version-number in camelcase
-console.log(logo.dataset.versionNumber);
-
-// * Classes 
-logo.classList.add('c', 'j');
-logo.classList.remove('c');
-logo.classList.toggle('c');
-logo.classList.contains('c');
-
-// Don't use - it overrides all classes at once
-logo.className = 'jonas';
-
 // -------------------
 // *Implementing Smooth Scrolling
-
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
 
 // Button scrolling
 btnScrollTo.addEventListener('click', function(e) {
@@ -173,50 +65,196 @@ btnScrollTo.addEventListener('click', function(e) {
 
 });
 
-//* Events and events handlers
 
-const h1 = document.querySelector('h1');
+//  //
+//  * Page navigation - /////////////////////////////////////////////////////////////////////////////////////////////////
 
-const alertH1 = function(e) {
-    alert('You have activated the mouseenter 🐭');
-    // To remove the event once it has been executed we use:
-    // h1.remove('mouseenter', alertH1);
-}
+//? NOT EFFICIENT - If we attach an eventhandler to 10k elements as below the performance will be affected, as we will create 10k copies of the same function. So we use, event delegation in option B
 
-h1.addEventListener('mouseenter', alertH1);
+// A
+// document.querySelectorAll('.nav__link').forEach((element) => {
+//     console.log(element);
+//     element.addEventListener('click', function(e) {
+//         e.preventDefault();
+//         console.log(element);
+//         // this is the current element
+//         const id = this.getAttribute('href');
+//         console.log(id);
+//         document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//     });
+// });
 
-setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
-
-//* property onmouseenter
-
-// h1.onmouseenter = function(e) {
-//     alert('You have activated the onmouseenter 🐭');
-// }
-
-const randomInt = (min, max) =>
-    Math.floor(Math.random() * (max - min + 1) + min);
-const randomColor = () => `rgb(${randomInt(0,255)}, ${randomInt(0,255)}, ${randomInt(0,255)})`;
-console.log(randomColor(0, 255));
-
-document.querySelector('.nav__link').addEventListener('click', function(e) {
-    this.style.backgroundColor = randomColor();
-    // Where the event (click) happened not where it is attached
-    console.log('LINK', e.target, e.currentTarget);
-
-    // Stop propagation
-    // e.stopPropagation();
-
-});
+// B - event delegation
+// 1. Add event listener to common parent element (nav__links)
+// 2. Determine what element originates the event 
 
 document.querySelector('.nav__links').addEventListener('click', function(e) {
-    this.style.backgroundColor = randomColor();
-    console.log('CONTAINER', e.target, e.currentTarget);
-
+    e.preventDefault();
+    console.log(e.target);
+    // Matching strategy - as if we click in nav__link it also does a log for it BUT we only want nav__links
+    if (e.target.classList.contains('nav__link')) {
+        const id = e.target.getAttribute('href');
+        console.log(id);
+        document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+    }
 });
 
-document.querySelector('.nav').addEventListener('click', function(e) {
-    this.style.backgroundColor = randomColor();
-    console.log('NAV', e.target, e.currentTarget);
 
-    // when the parameter is true, the event handler will no longer listen to bubbling events but rather to capturing events
-}, true);
+
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+// Selecting elements
+// console.log(document.documentElement); // all dom
+// console.log(document.head);
+// console.log(document.body);
+
+
+// // console.log();
+// const header = document.querySelector('.header');
+// const allSections = document.querySelectorAll('.section');
+
+// document.getElementById('section--1');
+// const allBtns = document.getElementsByTagName('button');
+// console.log(allBtns);
+
+//*  Creating and inserting Elements
+// .insertAdjacentHTML method takes two parameters: the position where you want to insert the HTML content and the actual HTML content you want to insert
+/*
+'beforebegin': Inserts the HTML content immediately before the element.
+'afterbegin': Inserts the HTML content at the beginning of the element.
+'beforeend': Inserts the HTML content at the end of the element.
+'afterend': Inserts the HTML content immediately after the element.
+*/
+//  Exemple
+// const element = document.getElementById('myElement');
+// const htmlContent = '<p>This is some text.</p>';
+
+// element.insertAdjacentHTML('afterend', htmlContent);
+
+
+//? The div has been created but it is not present in the DOM 
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+
+// message.textContent = 'We use cookies for improved functionality and analytics';
+
+// message.innerHTML =
+//     'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// ? We add the div after the header so that it will be present in the DOM - first child in header
+
+//?DOM elements are unique and can only exist in 1 place at a time, so it will prepend and then move to the append - Except if we copy it using clineNode(true) - true for the child elements
+
+// CHILD
+// header.prepend(message); // child beginning
+// header.append(message); // child end
+// header.append(message.cloneNode(true)); // child end
+
+// SIBLINGS
+// header.before(message); // sister before element
+// header.after(message); // sister after element
+
+// Delete Elements
+// document.querySelector('.btn--close-cookie').addEventListener('click', function() {
+//     message.remove();
+//     // Before they use to do DOM traversing to access the element, as you could not access it directly
+//     // message.parentElement.removeChild(message);
+// });
+
+// -------------------------------
+// * Style, attributes and classes
+
+// Styles
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '120%';
+
+// console.log(message.style.color);
+
+// // To get a style from an element - getComputedStyle()
+// console.log(getComputedStyle(message).color);
+// console.log(getComputedStyle(message).height);
+
+// // ! the height is not taken into account, idk why
+// message.style.height =
+//     Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+
+// // ? CUSTOM PROPERTIES - setProperty is the :root in .css to change several things at the same time
+// document.documentElement.style.setProperty('--color-primary', 'pink')
+
+
+// //* Attributes
+// const logo = document.querySelector('.nav__logo');
+// //* Classic ones - existe déjà
+// console.log(logo.alt);
+// console.log(logo.src); // shows the absolute link (same in href), you need a getAttribute to get the relative one, as show below
+// console.log(logo.getAttribute('src'));
+// console.log(logo.className);
+// // setting them:
+// logo.alt = 'Bautiful minimalist logo';
+
+// //* Non-standard - created by me
+// console.log(logo.designer); // 'undefined' - Not the way to access it as it is me that created this attribute
+// console.log(logo.getAttribute('designer')); // 'Elena' - correct way to get it
+// // setting them:
+// logo.setAttribute('company', 'Bankist');
+
+// //  dataset - to get from the attribute data : data-version-number="3.0", version-number in camelcase
+// console.log(logo.dataset.versionNumber);
+
+// // * Classes 
+// logo.classList.add('c', 'j');
+// logo.classList.remove('c');
+// logo.classList.toggle('c');
+// logo.classList.contains('c');
+
+// // Don't use - it overrides all classes at once
+// logo.className = 'jonas';
+
+
+// //* Events and events handlers
+
+// const h1 = document.querySelector('h1');
+
+// const alertH1 = function(e) {
+//     alert('You have activated the mouseenter 🐭');
+//     // To remove the event once it has been executed we use:
+//     // h1.remove('mouseenter', alertH1);
+// }
+
+// h1.addEventListener('mouseenter', alertH1);
+
+// setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// //* property onmouseenter
+
+// // h1.onmouseenter = function(e) {
+// //     alert('You have activated the onmouseenter 🐭');
+// // }
+
+// const randomInt = (min, max) =>
+//     Math.floor(Math.random() * (max - min + 1) + min);
+// const randomColor = () => `rgb(${randomInt(0,255)}, ${randomInt(0,255)}, ${randomInt(0,255)})`;
+// console.log(randomColor(0, 255));
+
+// document.querySelector('.nav__link').addEventListener('click', function(e) {
+//     this.style.backgroundColor = randomColor();
+//     // Where the event (click) happened not where it is attached
+//     console.log('LINK', e.target, e.currentTarget);
+
+//     // Stop propagation
+//     // e.stopPropagation();
+
+// });
+
+// document.querySelector('.nav__links').addEventListener('click', function(e) {
+//     this.style.backgroundColor = randomColor();
+//     console.log('CONTAINER', e.target, e.currentTarget);
+
+// });
+
+// document.querySelector('.nav').addEventListener('click', function(e) {
+//     this.style.backgroundColor = randomColor();
+//     console.log('NAV', e.target, e.currentTarget);
+
+//     // when the parameter is true, the event handler will no longer listen to bubbling events but rather to capturing events
+// }, true);
